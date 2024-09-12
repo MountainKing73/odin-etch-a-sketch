af291f0a62c80728ec04350ec7ae4b373348885f
James Cummings <jamescummings@gmail.com>
2024-09-12 19:02:36 -0400

    Added hover event

--- sketch.js
+++ sketch.js
@@ -3,5 +3,12 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.className = "box";
+  square.id = "box" + i;
   container.append(square);
+  addEventListener("mouseover", function (e) {
+    const box = document.getElementById(e.target.id);
+    if (box != null) {
+      box.style.backgroundColor = "red";
+    }
+  });
 }
