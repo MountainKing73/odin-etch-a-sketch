a4c6f5c2788d267b699ad91d52c030195925bf44
James Cummings <jamescummings@gmail.com>
2024-09-13 10:47:34 -0400

    change to random color

--- sketch.js
+++ sketch.js
@@ -14,7 +14,9 @@ function draw_grid(boxes) {
     square.addEventListener("mouseover", function (e) {
       const box = document.getElementById(e.target.id);
       if (box != null) {
-        box.style.backgroundColor = "red";
+        //box.style.backgroundColor = "red";
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        box.style.backgroundColor = "#" + randomColor;
       }
     });
   }
