b78ef0587cd8f204c5018531ae80207582a53e55
James Cummings <jamescummings@gmail.com>
2024-09-13 10:12:24 -0400

    added grid size button

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.className = "box";
-  square.id = "box" + i;
-  container.append(square);
-  addEventListener("mouseover", function (e) {
-    const box = document.getElementById(e.target.id);
-    if (box != null) {
-      box.style.backgroundColor = "red";
-    }
-  });
+function draw_grid(boxes) {
+  container.innerHTML = "";
+  for (let i = 0; i < boxes * boxes; i++) {
+    const boxSize = Math.floor(600 / boxes);
+    const square = document.createElement("div");
+    square.className = "box";
+    square.id = "box" + i;
+    square.style.backgroundColor = "#E8E8E8";
+    square.style.height = boxSize + "px";
+    square.style.width = boxSize + "px";
+    container.append(square);
+    square.addEventListener("mouseover", function (e) {
+      const box = document.getElementById(e.target.id);
+      if (box != null) {
+        box.style.backgroundColor = "red";
+      }
+    });
+  }
 }
+
+const button = document.querySelector("#newGrid");
+button.addEventListener("click", () => {
+  let gridSize = prompt("How many squares per side (1-100): ");
+  while (gridSize < 1 || gridSize > 100) {
+    gridSize = prompt("How many squares per side (1-100): ");
+  }
+
+  draw_grid(gridSize);
+});
+
+draw_grid(16);
